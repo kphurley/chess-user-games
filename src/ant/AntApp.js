4857b8b2e6d7ff830417fc3d0a89624abf64f3fd
Kevin Hurley <kphurley@gmail.com>
2020-07-27 14:09:59 -0500

    Add all initial libraries and basic structure

---
+++ src/ant/AntApp.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AntApp = () => <div>ANT</div>;
+
+export default AntApp;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
